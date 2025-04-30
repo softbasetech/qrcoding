9fab06b413698e1eaad4e39113a9ee930a15d311
princepatrickbondzie <ngekow@gmail.com>
2025-04-30 09:09:20 +0000

    qr code generation, image conversion, file conversions etc.

--- app/api/qr-code/[shortId]/stats/route.ts
+++ app/api/qr-code/[shortId]/stats/route.ts
@@ -7,11 +7,12 @@ import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(
   req: NextRequest,
-  { params }: { params: { shortId: string } }
+  { params } : { params: Promise<{ shortId: string }> }
 ) {
   try {
     await dbConnect();
-    const qrCode = await QRCode.findOne({ shortId: params.shortId });
+    const { shortId } = await params;
+    const qrCode = await QRCode.findOne({ shortId: shortId });
 
     if (!qrCode) {
       return NextResponse.json(
